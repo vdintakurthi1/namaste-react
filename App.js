d8de1c4188e4d64e5df9bf7f54ba1bd97a6d95ee
dintakurthiv <dintakurthiv@vmware.com>
2023-09-07 15:52:41 +0530

    episode-1

---
+++ App.js
@@ -0,0 +1,6 @@
+const heading = React.createElement("h1", {}, "hello varun from react");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
+
+console.log(heading)
